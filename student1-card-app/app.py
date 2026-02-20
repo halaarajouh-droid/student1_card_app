adc925f60afc9d5117d230936676fa36859671a5
halaarajouh-droid <halaarajouh@gmail.com>
2026-02-20 08:51:35 +0300

    Add files via upload

--- student1-card-app/app.py
+++ student1-card-app/app.py
@@ -1,9 +1,8 @@
-from flask import Flask ,render_template
+from flask import Flask , render_template,request
 import os 
 app=Flask(__name__)
 @app.route("/")
-def index():
+def home():
     return render_template("home.html")
 if __name__=="__main__":
-    port=int(os.environ.get("PORT",1000))
-    app.run(host="0.0.0.0",port=port,debug=True)
+    app.run(host="127.0.0.1",port=5000,debug=True)
